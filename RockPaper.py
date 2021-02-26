6509b58b3d0cd2da8488259df7e4dbc145857348
TaeLim <taelim.eeng@gmail.com>
2021-02-26 16:27:50 -0500

    Changes to RPS game
    Added winstatement and ask player option to play more

--- RockPaper.py
+++ RockPaper.py
@@ -4,28 +4,45 @@ import random
 from namequestion import askname
 from namequestion import alwaysloses
 
-#list of numbers played and won
-plays=0
-win=0
+lose = True
+
+#list of numbers played (plays[0]) and won (plays[1])
+plays=[0, 0]
 
 global name
-name = askname()
+name = askname() #ask player what their name is
 
 def score():
-    if plays <= 0:
+    if plays[0] <= 0:
         print("\n%s Lets play the game. Rock! paper! scissors!!!"%name)
     else:
-        print("You have played %d many times and won %d times"%(int(plays), int(win)))
+        print("You have played " + str(plays[0]) + " many times and won " + str(plays[1]) + " times")
 
 score()
 
-while True:
+def asking_to_continue():
+    plays[0] += 1
+    plays[1] += 1
+    while True:
+        continue_question = input("You win! Do you wish to continue? Y/N:  ")
+        questions = ["Y", "y", "n", "N"]
+        if continue_question not in questions:
+            print("Please type in Y/N")
+            continue
+        if str.upper(continue_question) == "N":
+            score()
+            exit()
+        else:
+            return True
+
+
+while asking_to_continue:
     userChoice = input("\nPlease choose one! [R]ock, [P]aper, or [S]cissors: ")
     inputlist = ["S", "s", "R", "r", "P", "p"]
 
     #if you did not choose one of the three letters
     if userChoice not in inputlist:
-        print("Please choose one of three letter in [ ]")
+        print("Please choose one of three letter")
         continue
 
     print("You chose: " + str.upper(userChoice))
@@ -38,25 +55,18 @@ while True:
 
     if myChoice == str.upper(userChoice):
         print("Tie!")
-        plays += 1
-        score()
+        plays[0] += 1
     elif myChoice == 'R' and userChoice.upper() == 'S':
         print("Scissors beats rock, I win! ")
-        plays += 1
-        score()
+        plays[0] += 1
         continue
     elif myChoice == 'S' and userChoice.upper() == 'P':
         print("Scissors beats paper! I win! ")
-        plays += 1
-        score()
+        plays[0] += 1
         continue
     elif myChoice == 'P' and userChoice.upper() == 'R':
         print("Paper beat rock, I win! ")
-        plays += 1
-        score()
+        plays[0] += 1
         continue
     else:
-        print("You win!")
-        plays += 1
-        win += 1
-        score()
+        asking_to_continue()
